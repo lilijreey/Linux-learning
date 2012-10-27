b55df051ad52e1fb58622a208c993302765edcfa
lili <lilijreey@gmail.com>
2012-10-27 22:50:06 +0800

    ci 2012-10-27

--- shared-lib/s1.c
+++ shared-lib/s1.c
@@ -12,6 +12,10 @@
  */
 
 #include <stdio.h>
-void s1() {
+void 
+//__attribute__ ((visibility("hidden"))) 如果不想让外部见到shared-lib 的内部借口
+//可以使用这个属性, hidden属性可以在shared-lib 内部使用
+s1()
+{
     printf("s1.c s1 function\n");
 }
