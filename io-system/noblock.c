7a6cf5549fc5394228d6910eca8547146568afb1
lili <lilijreey@gmail.com>
2013-04-22 11:45:35 +0800

    ci 2013-04-22

---
+++ io-system/noblock.c
@@ -0,0 +1,63 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  14-1-noblock.c
+ *
+ *    Description:  
+ *
+ *        Created:  12/26/2011 11:49:24 PM
+ *       Compiler:  gcc4.4
+ *
+ *         Author:  first name surname (), lilijreey@Gmail.com
+ *
+ * =====================================================================================
+ */
+#include	<stdio.h>
+#include	<fcntl.h>
+#include	<errno.h>
+#include	<unistd.h>
+#include	<zhao/tools.h>
+
+char buf[50000] ;
+
+int main() {
+
+	int n2write, nwrite;
+	char *ptr;
+		int		val;
+
+	n2write = read(STDIN_FILENO, buf, sizeof(buf));
+	fprintf(stderr, "Raed %d bytes\n", n2write) ;
+
+
+	if ((val = fcntl(STDOUT_FILENO, F_GETFL, 0)) < 0)
+		perror("fcntl F_GETFL error");
+
+	val |= O_NONBLOCK;		/* turn on flags */
+
+	if (fcntl(STDOUT_FILENO, F_SETFL, val) < 0)
+		perror("fcntl F_SETFL error");
+
+	ptr = buf;
+	while (n2write > 0) {
+		errno=0;
+		nwrite = write(STDOUT_FILENO, ptr, n2write) ;
+		fprintf(stderr, "nwrite = %d, errno=%d\n", nwrite, errno);
+
+		if (nwrite > 0) {
+			ptr += nwrite ;
+			n2write -= nwrite ;
+		}
+	}
+
+	if ((val = fcntl(STDOUT_FILENO, F_GETFL, 0)) < 0)
+		perror("fcntl F_GETFL error");
+
+	val &= ~O_NONBLOCK;		/* turn flags off */
+
+	if (fcntl(STDOUT_FILENO, F_SETFL, val) < 0)
+		perror("fcntl F_SETFL error");
+
+	return 0 ;
+}
+
