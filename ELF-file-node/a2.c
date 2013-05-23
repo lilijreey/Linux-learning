af3d4545e2cde7c8b532f0ef39fe0d9a452a97ce
lili <lilijreey@gmail.com>
2013-05-24 07:41:48 +0800

    ci 2013-05-24

---
+++ ELF-file-node/a2.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  a2.c
+ *
+ *    Description:  
+ *
+ *        Created:  02/21/2012 08:40:39 AM
+ *       Compiler:  gcc4.4
+ *
+ *         Author:  first name surname (), lilijreey@Gmail.com
+ *
+ * =====================================================================================
+ */
+#include	<stdio.h>
+//#include	<unistd.h>
+//#include	<stdlib.h>
+//#include	<string.h>
+//#include	<zhao/tools.h>
+
+static void a() {
+	printf("a2.c\n");
+}
