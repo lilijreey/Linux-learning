1ba3aebed5d49d2c9a307f5ca13907d052a2015f
lili <lilijreey@gmail.com>
2014-03-07 11:25:13 +0800

    ci

--- pthread/buf.c
+++ pthread/buf.c
@@ -14,45 +14,161 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
-       #include <sys/time.h>
+#include <sys/time.h>
 
-#define	FULL 1024 * 1024			/*  */
+#define	FULL 1024 * 1024/*  */
+#define	BF 128			/*  */
+
+typedef struct car_s{
+ pthread_mutex_t *lock;
+ pthread_cond_t *empty;
+
+ int fd;
+ int tail;
+ struct car_s *next; 
+ void *buf;
+
+} car;
+
+static car *xx;
+
+typedef struct event_s
+{
+    char type; //'f' full, 'c' close
+    car *c;
+} event_t;
+
 
 struct {
     pthread_mutex_t lock;
-    pthread_cond_t full_cond;
-    pthread_cond_t empty_cond;
+    pthread_mutex_t list_lock;
+    pthread_cond_t event;
 
+    car *list; 
     int head;
     int tail;
     int size;
     void *data;
-    int reset;
 } buf = {.lock=PTHREAD_MUTEX_INITIALIZER, 
-    .full_cond =PTHREAD_COND_INITIALIZER,
-    .empty_cond =PTHREAD_COND_INITIALIZER
+    .list_lock=PTHREAD_MUTEX_INITIALIZER,
+    .event=PTHREAD_COND_INITIALIZER,
+    .list=NULL,
 };
+
+car* new_buf(const char *file)
+{
+    pthread_mutex_t *lock;
+    pthread_cond_t *full;
+    pthread_cond_t *empty;
+
+    int fd = open(file, O_CREAT | O_RDWR | O_CLOEXEC | O_TRUNC , 0777);
+    if (fd == -1) {
+        printf("open bad\n");
+        return NULL;
+    }
+
+    car *c = malloc(sizeof (car));
+
+    lock = malloc(sizeof (pthread_mutex_t));
+    full = malloc(sizeof (pthread_cond_t));
+    empty = malloc(sizeof (pthread_cond_t));
+
+    pthread_mutex_init(lock, NULL);
+    pthread_cond_init(full, NULL);
+    pthread_cond_init(empty, NULL);
+
+    c->buf= malloc(FULL);
+    c->tail = 0;
+
+    c->lock = lock;
+    c->empty= empty;
+
+    c->fd = fd;
+
+    pthread_mutex_lock(&buf.list_lock);
+    c->next=buf.list;
+    buf.list = c;
+    pthread_mutex_unlock(&buf.list_lock);
+
+    return c;
+}
+
+
+void rm_from_list(car *c)
+{
+    if (buf.list == c) {
+        buf.list = c->next;
+        return;
+    }
+
+    car *n=buf.list;
+    while (n->next != c && n->next != NULL)
+        n = n->next;
+
+    if (n->next == NULL) {
+        assert(0);
+        return;
+    }
+
+    n->next = c->next;
+}
+
+void flush_log(car *c)
+{
+    int n;
+    assert(c->fd);
+
+    pthread_mutex_lock(c->lock);
+    if (c->tail != 0) {
+        n = write(c->fd, c->buf, c->tail);
+        if (n == -1)
+            perror("xxx write failed");
+        c->tail = 0;
+    }
+    pthread_mutex_unlock(c->lock);
+
+}
+
+void car_destroy(car *c)
+{
+                rm_from_list(c); 
+                flush_log(c);
+                pthread_cond_destroy(c->empty); 
+                pthread_mutex_destroy(c->lock);
+
+                if (c->fd > 2)
+                    close(c->fd);
+
+                free(c->buf);
+                free(c);
+
+}
 
 void log_cancel(void *x)
 {
-    printf("ener cancel\n");
-    int *fd = (int *)x;
-    if (*fd == -1)
-        return;
-
-    if (buf.head != buf.tail) {
-        int n = write(*fd, buf.data+buf.head, buf.tail - buf.head);
-        if (n == -1)
-            printf("%s\n", strerror(errno));
-        else
-            printf("logg write %d head:%d tail:%d\n", n, buf.head, buf.tail);
-
-        buf.head = buf.tail = 0;
-    }
-    close(*fd);
-
     printf("log cancel\n");
+    car *c= buf.list;
+    while (c) {
+        printf("des :%d\n", c->fd);
+        car_destroy(c);
+        c=c->next;
+    }
+
 }
+
+
+
+
+void flush_logs()
+{
+    car *c= buf.list;
+    while (c) {
+        flush_log(c);
+        c=c->next;
+    }
+}
+
+
 
 void * log_back(void *arg)
 {
@@ -60,39 +176,64 @@ void * log_back(void *arg)
 
     struct timeval now;
     struct timespec timeout;
+    event_t *e;
+    car *c;
+    int n=0, ret=0;
+    char arr[BF];
+    int bl, hl;
 
     printf("log_back start\n");
 
 	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
     pthread_cleanup_push(log_cancel, &fd);
-    fd = open("./log", O_CREAT | O_RDWR | O_CLOEXEC | O_TRUNC , 0777);
-    if (fd == -1) {
-        printf("open bad\n");
-        return NULL;
-    }
 
     //write 
     while (1) {
-        pthread_mutex_lock(&buf.lock);
-
         gettimeofday(&now, NULL);
         timeout.tv_sec = now.tv_sec+1;
         timeout.tv_nsec = now.tv_usec * 1000;
 
-        while (buf.head == buf.tail) {
-            pthread_cond_broadcast(&buf.empty_cond);
-            pthread_cond_timedwait(&buf.full_cond, &buf.lock, &timeout);
+        pthread_mutex_lock(&buf.lock);
+        while (buf.head == buf.tail && ret != ETIMEDOUT)   {
+            printf("event wait\n");
+            ret = pthread_cond_timedwait(&buf.event, &buf.lock, &timeout);
         }
+        ret=0;
 
-//        h=buf.head; t=buf.tail;
-        int n = write(fd, buf.data+buf.head, buf.tail - buf.head);
+        //dup event
+        memcpy(arr, buf.data, buf.tail);
+        bl = buf.tail;
+        hl=0;
         buf.head = buf.tail = 0;
+
         pthread_mutex_unlock(&buf.lock);
 
-        if (n == -1)
-            printf("%s\n", strerror(errno));
-        else
-            printf("logg write %d head:%d tail:%d\n", n, h, t);
+
+
+        while (hl != bl) { //pop event 
+            e= (event_t *)(arr+hl);
+            hl += sizeof (event_t);
+
+            switch (e->type) {
+            case 'c':
+                car_destroy(e->c); 
+                break;
+            case 'f':
+                c=e->c;
+                pthread_mutex_lock(c->lock);
+                n = write(c->fd, c->buf, c->tail);
+                if (n == -1)
+                    perror("write failed");
+                c->tail=0;
+                pthread_mutex_unlock(c->lock);
+//                printf("broadcast\n");
+                pthread_cond_broadcast(c->empty);
+                break;
+            }
+        }
+
+//        printf("llllllllllll fuls\n");
+        flush_logs();
 
     }
 
@@ -102,19 +243,47 @@ void * log_back(void *arg)
 }
 
 
-void write_log(const char* msg, size_t len)
+void push_full_event(car *c)
 {
+//    printf("push full tail:%d\n", buf.tail);
+    event_t *e;
+    // TODO buf even full condition ??
     pthread_mutex_lock(&buf.lock);
-    while (buf.tail + len > buf.size) {
+    e = (event_t*)(buf.data + buf.tail);
+    e->c=c;
+    e->type='f';
+    buf.tail += sizeof(event_t);
+    pthread_mutex_unlock(&buf.lock);
+}
+
+void push_close_event(car *c)
+{
+    printf("close full\n");
+    event_t *e;
+    //buf even full condition ??
+    pthread_mutex_lock(&buf.lock);
+    e = (event_t*)(buf.data + buf.tail);
+    e->c=c;
+    e->type='c';
+    buf.tail += sizeof(event_t);
+    pthread_mutex_unlock(&buf.lock);
+}
+
+void write_log(car *c, const char* msg, size_t len)
+{
+    pthread_mutex_lock(c->lock);
+    while (c->tail + len > FULL) {
         printf("buf full work down.....\n");
-        pthread_cond_signal(&buf.full_cond);
-        pthread_cond_wait(&buf.empty_cond, &buf.lock);
+        push_full_event(c);
+        pthread_cond_signal(&buf.event);
+        pthread_cond_wait(c->empty, c->lock);
+        printf("work buf empty\n");
     }
 
-    memcpy(buf.data+buf.tail, msg, len);
-    buf.tail += len;
+    memcpy(c->buf +c->tail, msg, len);
+    c->tail += len;
 
-    pthread_mutex_unlock(&buf.lock);
+    pthread_mutex_unlock(c->lock);
 }
 
 void* work(void *arg)
@@ -122,12 +291,12 @@ void* work(void *arg)
     char buf[64];
     printf("work %lu start\n", pthread_self());
 
-    for (int i=0; i< 500000; ++i) {
+    for (int i=0; i< 50; ++i) {
         int len = snprintf(buf, 64, "thread %lu write a log\n", (unsigned long)pthread_self());
         assert(len < 64);
         buf[len++] = '\0';
 
-        write_log(buf, len);
+        write_log(xx, buf, len);
     }
 
     return NULL;
@@ -137,12 +306,16 @@ void* work(void *arg)
 int
 main(int argc, char *argv[])
 {
-    buf.data = malloc(FULL);
+    buf.data = malloc(BF);
     buf.head = buf.tail = 0;
-    buf.size = FULL;
+    buf.size = BF;
+    buf.list = NULL;
 
 	pthread_t ptid[2];
 	pthread_t logtid;
+
+    xx = new_buf("./log");
+    assert(xx);
 
     (pthread_create(&logtid, NULL, log_back, NULL));
     (pthread_create(&ptid[0], NULL, work, NULL));
@@ -152,6 +325,7 @@ main(int argc, char *argv[])
         pthread_join(ptid[i], NULL/*not care thread return code */);
 
     printf("work over\n");
+    sleep(100);
 
     //close log
     pthread_cancel(logtid);
